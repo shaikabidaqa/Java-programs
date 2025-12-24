816a2fbb05d8210e6a45dc980e36505f1e8e7ad6
Abidunnisa Shaik <shaikabida789@gmail.com>
2025-12-24 16:52:56 +0530

    Create SortingStrings.java

---
+++ src/strings/SortingStrings.java
@@ -0,0 +1,15 @@
+	package Arrayprogramms;
+
+import java.util.Arrays;
+
+public class SortingStrings {
+
+	public static void main(String[] args) {
+		String s[]= {"scott","marry","David","john"};
+		System.out.println("Before Sorting...."+ Arrays.toString(s));
+		Arrays.sort(s);
+		System.out.println("After Sorting...."+ Arrays.toString(s));
+	
+	}
+
+}
